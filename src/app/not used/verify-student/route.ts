ee32b2b98faebe73686e26a9500d59a71be2382e
snowflower204 <butterflyhann@gmail.com>
2025-05-11 12:55:06 +0800

    change

--- src/app/not used/verify-student/route.ts
+++ src/app/not used/verify-student/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from 'next/server';
+{/*import { NextResponse } from 'next/server';
 import mysql from 'mysql2/promise';
 
 export async function POST(request: Request) {
@@ -34,3 +34,4 @@ export async function POST(request: Request) {
     );
   }
 }
+*/}
